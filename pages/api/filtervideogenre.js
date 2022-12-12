7b2615491b73a277694bfc8af874edf60fc615f1
Jidraff Gathura <jidraff.social@gmail.com>
2022-12-12 18:54:09 +0300

    removed authentication check from pages/api/filtervideogenre because a user is not required to be logged in to filter for videos using genres

--- pages/api/filtervideogenre.js
+++ pages/api/filtervideogenre.js
@@ -2,16 +2,16 @@ import cookie from 'cookie';
 
 export default async (req, res) => {
     if (req.method === 'GET') {
-        const cookies = cookie.parse(req.headers.cookie ?? ' ');
-        const access = cookies.access ?? false
+        // const cookies = cookie.parse(req.headers.cookie ?? ' ');
+        // const access = cookies.access ?? false
         const genreId = req.query.genre_id
 
-        if (access === false) {
-            return res.status(401).json({
-                error: 'User unauthorized to make this request'
-            });
+        // if (access === false) {
+        //     return res.status(401).json({
+        //         error: 'User unauthorized to make this request'
+        //     });
 
-        }
+        // }
 
         try {
             const apiResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/store/videos/?genre=${genreId}`, {
