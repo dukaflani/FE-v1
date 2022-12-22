2cba21b55c72d611d2e1d8b717ad6fd1ee526258
Jidraff Gathura <jidraff.social@gmail.com>
2022-12-22 16:21:52 +0300

    added ability to add a view once albumtrack video is clicked

--- components/AlbumTrack.js
+++ components/AlbumTrack.js
@@ -1,8 +1,30 @@
 import React from 'react'
-import Link from 'next/link'
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 import { PlayPauseIcon } from '@heroicons/react/24/solid'
+import { useAddViewMutation } from '../redux/features/videos/videosApiSlice';
 
 const AlbumTrack = ({ track }) => {
+  const router = useRouter()
+    const [ addView ] = useAddViewMutation();
+    const { user } = useSelector((state) => state.auth)
+    const currentUser = user?.info?.id
+
+    const videoId = track?.video_id
+    const user_id = currentUser ? currentUser : 1
+
+    const newView = {
+        "video": videoId,
+        "user": user_id
+    }
+
+    const handlePlayVideo = async (id) => {
+      await addView(newView);
+      router.push({
+          pathname: `/watch/`,
+          query: { v: id, tab: "links" },
+        });
+  }
   
   return (
     <div className='p-1 mb-4 flex items-center justify-center'>
@@ -13,7 +35,7 @@ const AlbumTrack = ({ track }) => {
         </div>
         <div className='w-2/12 flex items-center justify-center'>
           {track?.video ? (
-            <Link href={{pathname: `/watch/`, query: {v: track?.url_id, tab: "links"}}} >< PlayPauseIcon className='h-5 w-5 text-blue-600 cursor-pointer' /></Link> 
+            <div onClick={() => handlePlayVideo(track?.url_id)} >< PlayPauseIcon className='h-5 w-5 text-blue-600 cursor-pointer' /></div> 
           ) : (
             <div>< PlayPauseIcon className='h-5 w-5 text-gray-100' /></div>
           )}
