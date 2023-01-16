2eefb16a3fbfcd4328287f1e026cbda02ed9cee3
Jidraff Gathura <jidraff.social@gmail.com>
2023-01-16 11:48:48 +0300

    removed pwa object and reconfigured next config file because it caused the build to fail in varcel

--- next.config.js
+++ next.config.js
@@ -3,13 +3,11 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-  // reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  },
+  reactStrictMode: true,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
   images: {
     domains: [process.env.NEXT_PUBLIC_IMAGES_URL],
     // unoptimized: true,
