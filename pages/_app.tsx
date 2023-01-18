edd0b55ab1d3ebf66f8dc1de7700e74b5e14e8b3
Jidraff Gathura <jidraff.social@gmail.com>
2023-01-18 18:34:10 +0300

    added google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import Head from 'next/head'
+import TagManager, { TagManagerArgs } from 'react-gtm-module'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import { store } from '../redux/app/store'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || " ";
+  const tagManagerArgs : TagManagerArgs = {
+    gtmId,
+  };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, [])
+  
+
+
+
   return (
     <>
     <Head>
