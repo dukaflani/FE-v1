ffd7f64e8ccbf6aa4e13da0246a680e8a83c3c56
Jidraff Gathura <jidraff.social@gmail.com>
2023-01-20 20:25:48 +0300

    added genres

--- data/genres.js
+++ data/genres.js
@@ -1,56 +1,176 @@
 export const genres = [
     {
-        id: 1,
+        id: 2,
         title: 'Kapuka',
         querySlug: 'kapuka',
 
     },
     {
-        id: 2,
+        id: 3,
         title: 'Gengetone',
         querySlug: 'gengetone',
 
     },
     {
-        id: 3,
+        id: 4,
         title: 'Amapiano',
         querySlug: 'amapiano',
 
     },
     {
-        id: 4,
+        id: 5,
         title: 'Swahili',
         querySlug: 'swahili',
 
     },
     {
-        id: 5,
+        id: 6,
         title: 'Bongo',
         querySlug: 'bongo',
 
     },
     {
-        id: 6,
+        id: 11,
+        title: 'Boomba',
+        querySlug: 'boomba',
+
+    },
+    {
+        id: 7,
         title: 'Ugaflow',
         querySlug: 'ugaflow',
 
     },
     {
-        id: 7,
+        id: 8,
         title: 'Rhumba',
         querySlug: 'rhumba',
 
     },
     {
-        id: 8,
+        id: 9,
         title: 'Afro Beat',
         querySlug: 'afro-beat',
 
     },
     {
-        id: 9,
+        id: 10,
         title: 'Genge',
         querySlug: 'genge',
+
+    },
+    {
+        id: 12,
+        title: 'Kwaito',
+        querySlug: 'kwaito',
+
+    },
+    {
+        id: 13,
+        title: 'Lingala',
+        querySlug: 'lingala',
+
+    },
+    {
+        id: 14,
+        title: 'Dancehall',
+        querySlug: 'dancehall',
+
+    },
+    {
+        id: 15,
+        title: 'Reggae',
+        querySlug: 'reggae',
+
+    },
+    {
+        id: 16,
+        title: 'Rap',
+        querySlug: 'rap',
+
+    },
+    {
+        id: 17,
+        title: 'Hip Hop',
+        querySlug: 'hip-hop',
+
+    },
+    {
+        id: 18,
+        title: 'House',
+        querySlug: 'house',
+
+    },
+    {
+        id: 19,
+        title: 'Dance',
+        querySlug: 'dance',
+
+    },
+    {
+        id: 20,
+        title: 'Drill',
+        querySlug: 'drill',
+
+    },
+    {
+        id: 21,
+        title: 'Trap',
+        querySlug: 'tarp',
+
+    },
+    {
+        id: 22,
+        title: 'Country',
+        querySlug: 'country',
+
+    },
+    {
+        id: 23,
+        title: 'Folk',
+        querySlug: 'folk',
+
+    },
+    {
+        id: 24,
+        title: 'Mugithi',
+        querySlug: 'mugithi',
+
+    },
+    {
+        id: 25,
+        title: 'Ohangala',
+        querySlug: 'ohangala',
+
+    },
+    {
+        id: 26,
+        title: 'Azonto',
+        querySlug: 'azonto',
+
+    },
+    {
+        id: 27,
+        title: 'Benga',
+        querySlug: 'benga',
+
+    },
+    {
+        id: 28,
+        title: 'Coupe Decale',
+        querySlug: 'coupe-decale',
+
+    },
+    {
+        id: 29,
+        title: 'Taarab',
+        querySlug: 'taarab',
+
+    },
+    {
+        id: 30,
+        title: 'Soukous',
+        querySlug: 'soukous',
 
     },
 ]
